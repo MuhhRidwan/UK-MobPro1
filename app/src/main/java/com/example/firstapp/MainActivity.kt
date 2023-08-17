f15819bdfe7b2e14d03e3bb4af47ab6ef024cae9
Muhammad Ridwan <mridwanrachmat07@gmail.com>
2023-08-17 16:32:45 +0700

    Menambahkan Tombol Next

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.firstapp
 
 import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
+import android.widget.Button
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val nextButton: Button = findViewById(R.id.nextButton)
+        nextButton.setOnClickListener { showNext() }
+    }
+
+    private fun showNext() {
+        Log.d("MainActivity", "Tombol diklik!")
     }
 }
