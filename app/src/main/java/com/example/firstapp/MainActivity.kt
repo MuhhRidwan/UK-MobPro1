8ddd838a35a311bb25296974004f8ccb8ebda0bc
Muhammad Ridwan <mridwanrachmat07@gmail.com>
2023-08-31 18:38:29 +0700

    Refactor fitur hitung BMI ke fragment

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -11,65 +11,8 @@ import com.example.firstapp.model.HasilBmi
 import com.example.firstapp.model.KategoriBmi
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-    private val viewModel: MainViewModel by lazy {
-        ViewModelProvider(this)[MainViewModel::class.java]
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        binding.button.setOnClickListener { hitungBmi() }
-        binding.button3.setOnClickListener { reset() }
-        viewModel.getHasilBmi().observe(this, { showResult(it) })
-    }
-    private fun reset() {
-        binding.beratBadanInp.setText("");
-        binding.tinggiBadanInp.setText("");
-        binding.bmiTextView.setText("");
-        binding.radioGroup.clearCheck()
-        binding.kategoriTextView.setText("");
-    }
-    private fun hitungBmi() {
-        val berat = binding.beratBadanInp.text.toString()
-        if (TextUtils.isEmpty(berat)) {
-            Toast.makeText(this, R.string.berat_invalid, Toast.LENGTH_LONG).show()
-            return
-        }
-        val tinggi = binding.tinggiBadanInp.text.toString()
-        if (TextUtils.isEmpty(tinggi)) {
-            Toast.makeText(this, R.string.tinggi_invalid, Toast.LENGTH_LONG).show()
-            return
-        }
-
-        val selectedId = binding.radioGroup.checkedRadioButtonId
-        if (selectedId == -1) {
-            Toast.makeText(this, R.string.gender_invalid, Toast.LENGTH_LONG).show()
-            return
-        }
-        viewModel.hitungBmi(
-            berat.toFloat(),
-            tinggi.toFloat(),
-            selectedId == R.id.priaRadioButton
-        )
-
-    }
-    private fun getKategoriLabel(kategori: KategoriBmi): String {
-        val stringRes = when (kategori) {
-            KategoriBmi.KURUS -> R.string.kurus
-            KategoriBmi.IDEAL -> R.string.ideal
-            KategoriBmi.GEMUK -> R.string.gemuk
-        }
-        return getString(stringRes)
-    }
-
-    private fun showResult(result: HasilBmi?) {
-        if (result == null) return
-        binding.bmiTextView.text = getString(R.string.bmi_x, result.bmi)
-        binding.kategoriTextView.text = getString(
-            R.string.kategori_x,
-            getKategoriLabel(result.kategori))
+        setContentView(R.layout.activity_main)
     }
 }
---
+++ app/src/main/java/com/example/firstapp/ui/HitungFragment.kt
@@ -0,0 +1,86 @@
+package com.example.firstapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModelProvider
+import com.example.firstapp.MainViewModel
+import com.example.firstapp.R
+import com.example.firstapp.databinding.ActivityMainBinding
+import com.example.firstapp.databinding.FragmentHitungBinding
+import com.example.firstapp.model.HasilBmi
+import com.example.firstapp.model.KategoriBmi
+
+class HitungFragment : Fragment() {
+
+    private lateinit var binding: FragmentHitungBinding
+
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(requireActivity())[MainViewModel::class.java]
+    }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+
+        binding = FragmentHitungBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.button.setOnClickListener { hitungBmi() }
+        viewModel.getHasilBmi().observe(requireActivity(), { showResult(it) })
+    }
+
+    private fun reset() {
+        binding.beratBadanInp.setText("");
+        binding.tinggiBadanInp.setText("");
+        binding.bmiTextView.setText("");
+        binding.radioGroup.clearCheck()
+        binding.kategoriTextView.setText("");
+    }
+    private fun hitungBmi() {
+        val berat = binding.beratBadanInp.text.toString()
+        if (TextUtils.isEmpty(berat)) {
+            Toast.makeText(context, R.string.berat_invalid, Toast.LENGTH_LONG).show()
+            return
+        }
+        val tinggi = binding.tinggiBadanInp.text.toString()
+        if (TextUtils.isEmpty(tinggi)) {
+            Toast.makeText(context, R.string.tinggi_invalid, Toast.LENGTH_LONG).show()
+            return
+        }
+
+        val selectedId = binding.radioGroup.checkedRadioButtonId
+        if (selectedId == -1) {
+            Toast.makeText(context, R.string.gender_invalid, Toast.LENGTH_LONG).show()
+
+            return
+        }
+        viewModel.hitungBmi(
+            berat.toFloat(),
+            tinggi.toFloat(),
+            selectedId == R.id.priaRadioButton
+        )
+
+    }
+    private fun getKategoriLabel(kategori: KategoriBmi): String {
+        val stringRes = when (kategori) {
+            KategoriBmi.KURUS -> R.string.kurus
+            KategoriBmi.IDEAL -> R.string.ideal
+            KategoriBmi.GEMUK -> R.string.gemuk
+        }
+        return getString(stringRes)
+    }
+
+    private fun showResult(result: HasilBmi?) {
+        if (result == null) return
+        binding.bmiTextView.text = getString(R.string.bmi_x, result.bmi)
+        binding.kategoriTextView.text = getString(
+            R.string.kategori_x,
+            getKategoriLabel(result.kategori))
+    }
+}
