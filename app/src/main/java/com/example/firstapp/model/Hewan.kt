84440337302f890454d1f7cd9701589434951bf4
Muhammad Ridwan <mridwanrachmat07@gmail.com>
2023-08-31 11:09:44 +0700

    Memproses data internet sebagai JSON

--- app/src/main/java/com/example/firstapp/model/Hewan.kt
+++ app/src/main/java/com/example/firstapp/model/Hewan.kt
@@ -1,8 +1,10 @@
 package com.example.firstapp.model
 
+import com.example.firstapp.R
+
 data class Hewan(
     val nama: String,
     val namaLatin: String,
-    val imageResId: Int
+    val imageResId: Int = R.drawable.angsa
 
 )
--- app/src/main/java/com/example/firstapp/network/HewanApiService.kt
+++ app/src/main/java/com/example/firstapp/network/HewanApiService.kt
@@ -1,14 +1,20 @@
 package com.example.firstapp.network
 
+import com.example.firstapp.model.Hewan
+
 private const val BASE_URL = "https://raw.githubusercontent.com/" +
         "indraazimi/galeri-hewan/static-api/"
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 interface HewanApiService {
     @GET("static-api.json")
-    suspend fun getHewan(): String
+    suspend fun getHewan(): List<Hewan>
 }
 object HewanApi {
     val service: HewanApiService by lazy {
